bae3e2d4a94a10080c8806674c8098f93242dac4
apewithtypewriter <159093079+apewithtypewriter@users.noreply.github.com>
2024-02-05 21:20:44 -0500

    Created docstring

--- sound/sound.py
+++ sound/sound.py
@@ -5,7 +5,26 @@ from scipy import signal
 
 
 class Sound:
-    """
+    """Create and modify a sound tone with specified properties
+    
+    Sounds are the basic building blocks of synsizer synthesis. They
+    are added to Clips in order to create a sound clip.
+    
+    Keyword arguments:
+    start       -- where in a Clip you wish the Sound to begin
+    duration    -- length of time (in seconds) the sound lasts
+    amplitude   -- the loudness (float from 0.0 to 1.0)
+    freq        -- frequency (pitch in Hz)
+    sample_rate -- samples (data points) per second (44100 Hz default)
+    envelope    -- an Envelope object that shapes the sound amplitude
+    filter      -- a Filter object that boosts or diminishes specified
+                   frequency ranges
+    
+    Methods:
+    add_waveform   -- add a soundwave to Sound data
+    apply_envelope -- modify Sound data with Envelope
+    apply_filter   -- modify Sound data with Filter
+    change_pitch   -- adjust the Sound frequency
     """
     
     def __init__(self, start, duration, freq, waveform, amplitude = 0.5,
--- sound/waves.py
+++ sound/waves.py
@@ -3,7 +3,19 @@ from scipy import signal
 
 
 def add_wave(data, amplitude, freq, waveform, sample_rate):
-    """
+    """Add a waveform to sound data
+    
+    Multiple waves may be added to a Sound. In that case, the principle
+    of the superposition of waves (add together amplitudes for each
+    corresponding time t).
+    
+    Waveform options: sin, saw, square, triangle
+    
+    Keyword arguments:
+    data        -- the audio data array (numpy array of floats)
+    amplitude   -- the loudness (float from 0.0 to 1.0)
+    freq        -- frequency (pitch in Hz)
+    sample_rate -- samples (data points) per second (44100 Hz default)
     """
     
     waveforms = {
