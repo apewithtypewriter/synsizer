073ba383cea01a644cdede306802c0ad8cfc98ac
apewithtypewriter <159093079+apewithtypewriter@users.noreply.github.com>
2024-02-05 20:54:25 -0500

    Added json config file

--- sound/config.py
+++ sound/config.py
@@ -5,5 +5,5 @@ import pathlib
 class Config:
     current_dir = pathlib.Path(__file__).parent.resolve()
     
-    with open(current_dir / "../../config.json") as config:
+    with open(current_dir / "../config.json") as config:
         defaults = json.load(config)["defaults"]
