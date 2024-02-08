d0af960502c638b5d65ba59d4105862f428197da
apewithtypewriter <159093079+apewithtypewriter@users.noreply.github.com>
2024-02-07 22:17:43 -0500

    Started json run function and json commands class

--- sound/run_json.py
+++ sound/run_json.py
@@ -1,5 +1,12 @@
+from commands import Commands.json_form
 import json
 
 
 def read(path):
-    pass
+    with open(path, "r") as file:
+        data = json.load(path)
+        
+    return data
+    
+def create(input_path):
+    data = read(input_path)
